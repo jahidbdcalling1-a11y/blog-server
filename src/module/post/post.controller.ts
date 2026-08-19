057c29678e6fd5825b0961390e96fd4f597fd334
Md Jahid <jahid.bdcalling1@gmail.com>
2026-08-19 17:00:23 +0600

    add the  new  function

--- src/module/post/post.controller.ts
+++ src/module/post/post.controller.ts
@@ -21,7 +21,14 @@ const createPost = catchAsync(async(req:Request, res:Response, next:NextFunction
 
 const getAllPost = catchAsync(async(req:Request, res:Response, next:NextFunction)=>{
 
-              const user  = await postService.getAllPost()
+              const page = Number(req.query.page) || 1;
+              const limit = Number(req.query.limit) || 10;
+              const search = (req.query.search as string)|| "";
+              const isFeatured = req.query.isFeatured ? req.query.isFeatured ==="true" : undefined;
+              const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+              const sort = (req.query.sort as string) || ""
+
+              const user  = await postService.getAllPost({page, limit, search, isFeatured, tags, sort })
                
               sentResponse(res,{
                          success:true,
--- src/module/post/post.service.ts
+++ src/module/post/post.service.ts
@@ -13,10 +13,55 @@ const postCreate = async (payload:Prisma.PostCreateInput): Promise<Post>=>{
 
 }
 
-const getAllPost = async ()=>{
-     const result = await prisma.post.findMany()
+const getAllPost = async ({page=1,limit = 10, search, isFeatured, tags, sort}:{page?:number, limit?:number, search?:string, isFeatured?:boolean, tags?:string[], sort?:string })=>{
 
-     return result
+      const skip = (page - 1) * limit
+
+      const where: any = {
+          AND:[
+            search && {
+                OR:[
+                     {
+                    title:{
+                        contains:search,
+                        mode:'insensitive'
+                    }
+                },
+                   {
+                    content:{
+                        contains:search,
+                        mode:'insensitive'
+                    }
+                }
+                ]
+            },
+            typeof isFeatured === "boolean" && {isFeatured},
+            (tags && tags.length >0) && {tags: { hasEvery:tags }}
+
+          ].filter(Boolean)
+      }
+  
+    const result = await prisma.post.findMany({
+         skip,
+         take: limit,
+         where,
+         orderBy:{
+            createdAt: sort === "asc" ? "asc" : "desc"
+         }
+    })
+
+    const total = await prisma.post.count({where})
+
+     return {
+      data:result,
+      pagination:{
+         page,
+         limit,
+         total,
+         totalPage: Math.ceil(total/ limit)
+      }
+
+     }
 }
 
 const getSinglePostId = async(id:number)=>{
